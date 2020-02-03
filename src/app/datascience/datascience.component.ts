3ab0d4d012f2cb889fb12780427ac2cfcf0ee78d
Ben <feba23@live.com>
2020-02-03 01:46:44 -0600

    Got datascience component to receive information from the backend

--- src/app/datascience/datascience.component.ts
+++ src/app/datascience/datascience.component.ts
@@ -16,17 +16,26 @@ export class DatascienceComponent implements OnInit {
   url: string = 'http://127.0.0.1:5000/';
 
   getData() {
+    let dat;
     console.log('here though');
 
     this.http.get(  this.url + 'file',
       { responseType: 'text' }).subscribe((data) => {
       parseString(data, { explicitArray: false }, (error, result) => {
-        this.data = JSON.stringify(data);
-        this.data = JSON.parse(this.data);
-        this.message = this.data;
-
+        dat = JSON.stringify(data);
+        dat = JSON.parse(dat);
+        this.message = dat.toString();
+        console.log(dat.type());
       });
     });
+    // console.log('Dat type: ');
+    // if (dat != undefined && dat != null){
+    //   this.message = dat.toString();
+    // }
+    // else {
+    //   this.message = "Didn't get dasfasfasfdasfasata";
+    // }
+
   }
 
   constructor(private http: HttpClient) {

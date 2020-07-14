5cf10539b4e01f44c490054b09dfeb27187d7962
Ben <feba23@live.com>
2020-07-14 01:59:04 -0500

    Started working on making navbar dynamically sticky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,37 @@ import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/c
 export class AppComponent implements OnInit {
   title = 'the strangest title';
 
-  constructor() { }
+  @ViewChild('navbar') header: ElementRef;
+  sticky = null;
+
+
+
+  // navStick() {
+  //   if (window.pageYOffset > this.sticky) {
+  //     this.header.classList.add("sticky");
+  //   } else {
+  //     this.header.classList.remove("sticky");
+  //   }
+  // }
+
+  constructor() {
+
+
+    console.log(this.header);
+    // console.log(this.header.toString());
+    // this.sticky = this.header.offsetTop;
+  }
 
   ngOnInit() {
+    // window.onscroll = this.navStick();
+
   }
+
+  // @HostListener('window:scroll', ['$event']) // for window scroll events
+  // onScroll(event) {
+  //   if(header.position){}
+  //   // console.log(event.toJSON().toString());
+  // }
 
 
 }

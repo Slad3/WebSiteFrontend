daea8a15eb4652d51edb5cb23c476841f1ee8287
ben <feba23@live.com>
2020-08-09 01:29:38 -0500

    Everything finally builds and publishes to github don't touch anything

--- updateSite.sh
+++ updateSite.sh
@@ -1,2 +1,2 @@
-ng build --prod --base-href 'https://slad3@github.io/benbarcaskey/'
-ngh --dir=dist/PersonalWebFrontend
+ng build --prod --build-optimizer=false --aot=false --base-href 'https://slad3.github.io/WebSiteFrontend/'
+ngh --dir=dist/benbarcaskey --no-silent

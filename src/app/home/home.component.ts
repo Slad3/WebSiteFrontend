3199a172ffc90582f06db1355775dd4f9564bdeb
Ben <feba23@live.com>
2020-08-22 18:59:43 -0500

    Added bootstrap to the project, and made the profile image more dynamic. The goal is to add bootstrap to all the components; it will be added in phases.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,28 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: [
+    './home.component.css',
+  ],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
 
   ngOnInit() {
-    var acc = document.getElementsByClassName("accordion");
-
+    var acc = document.getElementsByClassName('accordion');
 
     for (var i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
+      acc[i].addEventListener('click', function () {
+        this.classList.toggle('active');
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
           panel.style.maxHeight = null;
         } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
+          panel.style.maxHeight = panel.scrollHeight + 'px';
         }
       });
     }
   }
-
 }

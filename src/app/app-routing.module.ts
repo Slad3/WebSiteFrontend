f39a98c454726001a625756760f4492ff2ae322a
Ben <feba23@live.com>
2020-08-22 22:46:03 -0500

    Got frontend to send and recieve data from the backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DatascienceComponent } from './components/datascience/datascience.component';
 import { FeedComponent } from './components/feed/feed.component';
-import { ExperienceComponent } from './components/experience/experience.component'
-
+import { ExperienceComponent } from './components/experience/experience.component';
+import { FacebookDataAnalysisComponent } from './components/facebook-data-analysis/facebook-data-analysis.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'blog', component: FeedComponent },
-  { path: 'experience', component: ExperienceComponent},
-  { path: 'datascience', component: DatascienceComponent }
+  { path: 'experience', component: ExperienceComponent },
+  {
+    path: 'datascience',
+    component: DatascienceComponent,
+    children: [
+      // {path: '', },
+      { path: 'facebookAnalyzer', component: FacebookDataAnalysisComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/facebook-data-analysis/facebook-data-analysis.component.ts
+++ src/app/components/facebook-data-analysis/facebook-data-analysis.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Request } from '../../api/request.service';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpEventType,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { Observable, Subject } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-facebook-data-analysis',
   templateUrl: './facebook-data-analysis.component.html',
-  styleUrls: ['./facebook-data-analysis.component.css']
+  styleUrls: ['./facebook-data-analysis.component.css'],
 })
 export class FacebookDataAnalysisComponent implements OnInit {
+  data = 'asdfa';
+  backendUrl = 'http://localhost:8091/upload';
 
-  constructor() { }
+  form: FormGroup;
+  uploadStatus: Observable<number>;
 
-  ngOnInit(): void {
+  @Output() newResponse = new EventEmitter<string>();
+  fileNeeded = false;
+  toggle = false;
+
+  constructor(
+    private request: Request,
+    private http: HttpClient,
+	private formBuilder: FormBuilder,
+	private spinner: NgxSpinnerService
+  ) {
+	this.form = this.formBuilder.group({
+		file: ['']
+	  });
   }
+
+  ngOnInit(): void {}
+
+  onFileChange(event) {
+	  console.log("change")
+	  if (event.target.files && event.target.files[0]) {
+		const file = event.target.files[0];
+		this.form.get("file").setValue(file);
+	  }
+  }
+
+  onSubmitUpload() {
+	  console.log('submit')
+
+    if (this.form.get("file").value !== "") {
+      this.spinner.show();
+
+      const response = this.request.uploadFile(
+        this.form.get("file").value,
+        this.backendUrl
+	  );
+	  
+      this.uploadStatus = response.status;
+      response.file.subscribe((file) => {
+
+		this.newResponse.emit(file);
+		this.data = JSON.parse(file)
+		console.log(this.data)
+		this.toggle = true;
+      });
+    } else {
+		console.log("failed")
+      this.fileNeeded = false;
+    }
+  }
+
 
 }

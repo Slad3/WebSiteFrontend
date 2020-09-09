5e9ec2fdc35556bb398f52828b5ca61f1ca23f8e
Ben <feba23@live.com>
2020-09-09 03:03:53 -0500

    Imporved the sidebar

--- src/app/components/datascience/datascience.component.ts
+++ src/app/components/datascience/datascience.component.ts
@@ -1,4 +1,8 @@
-import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy,
+} from '@angular/common';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
@@ -7,27 +11,38 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./datascience.component.css'],
 })
 export class DatascienceComponent implements OnInit {
+  @ViewChild('sidenav') sideNav: ElementRef;
 
-	constructor(){
-		// this.route.parent.params.subscribe(params => console.log(params)); 
-	}
-	ngOnInit(){
-		let dropdown = document.getElementsByClassName("dropdown-btn")
-		let i;
-		for (i = 0; i < dropdown.length; i++) {
-			dropdown[i].addEventListener("click", function() {
-			  this.classList.toggle("active");
-			  var dropdownContent = this.nextElementSibling;
-			  if (dropdownContent.style.display === "block") {
-				dropdownContent.style.display = "none";
-			  } else {
-				dropdownContent.style.display = "block";
-			  }
-			});
-		  } 
-	}
+  constructor() {
+    // this.route.parent.params.subscribe(params => console.log(params));
+  }
+  ngOnInit() {
+    let dropdown = document.getElementsByClassName('dropdown-btn');
+    let i;
+    for (i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === 'block') {
+          dropdownContent.style.display = 'none';
+        } else {
+          dropdownContent.style.display = 'block';
+        }
+      });
+    }
+  }
 
+  openNav() {
+    console.log('asdfasdfsa');
+    document.getElementById('sidenav').style.width = '180px';
+    document.getElementById('sideButton').style.width = '0px';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+  }
 
-
-
+  closeNav() {
+    console.log('buttts');
+    document.getElementById('sidenav').style.width = '0px';
+    document.getElementById('sideButton').style.width = 'auto';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+  }
 }

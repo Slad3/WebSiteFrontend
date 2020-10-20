63a4a40f8d76362a294db244676c68237dd970c2
Ben <feba23@live.com>
2020-10-19 23:17:26 -0500

    Fixing the missing instructions issue

--- src/app/components/datascience/datascience.component.ts
+++ src/app/components/datascience/datascience.component.ts
@@ -19,39 +19,39 @@ import {
 	  // this.route.parent.params.subscribe(params => console.log(params));
 	}
 	ngOnInit() {
-	  document.getElementById('sidenav').style.left = '-2px';
-	  let dropdown = document.getElementsByClassName('dropdown-btn');
-	  let i;
-	  for (i = 0; i < dropdown.length; i++) {
-		dropdown[i].addEventListener('click', function () {
-		  this.classList.toggle('active');
-		  var dropdownContent = this.nextElementSibling;
-		  if (dropdownContent.style.display === 'block') {
-			dropdownContent.style.display = 'none';
-		  } else {
-			dropdownContent.style.display = 'block';
-		  }
-		});
-	  }
+	//   document.getElementById('sidenav').style.left = '-2px';
+	//   let dropdown = document.getElementsByClassName('dropdown-btn');
+	//   let i;
+	//   for (i = 0; i < dropdown.length; i++) {
+	// 	dropdown[i].addEventListener('click', function () {
+	// 	  this.classList.toggle('active');
+	// 	  var dropdownContent = this.nextElementSibling;
+	// 	  if (dropdownContent.style.display === 'block') {
+	// 		dropdownContent.style.display = 'none';
+	// 	  } else {
+	// 		dropdownContent.style.display = 'block';
+	// 	  }
+	// 	});
+	//   }
 	  
   
-	  this.sideNav = document.getElementById('sidenav') as HTMLElement;
-	  this.sticky = this.sideNav.offsetTop;
+	//   this.sideNav = document.getElementById('sidenav') as HTMLElement;
+	//   this.sticky = this.sideNav.offsetTop;
   
-	  window.addEventListener('scroll', (event) => {
-		if (window.pageYOffset >= this.sticky) {
-		  this.sideNav.classList.add('sticky');
-		} else {
-		  this.sideNav.classList.remove('sticky');
-		}
-	  });
+	//   window.addEventListener('scroll', (event) => {
+	// 	if (window.pageYOffset >= this.sticky) {
+	// 	  this.sideNav.classList.add('sticky');
+	// 	} else {
+	// 	  this.sideNav.classList.remove('sticky');
+	// 	}
+	//   });
   
-	  window.addEventListener('resize', (event) => {
+	//   window.addEventListener('resize', (event) => {
   
-		if (this.sideNav.offsetTop > 0) {
-		  this.sticky = this.sideNav.offsetTop;
-		}
-	  });
+	// 	if (this.sideNav.offsetTop > 0) {
+	// 	  this.sticky = this.sideNav.offsetTop;
+	// 	}
+	//   });
   
   
 	}

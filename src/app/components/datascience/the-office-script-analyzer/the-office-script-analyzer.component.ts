82c0432b67f9ea7117d8b30a54a7afb7b8bbda99
Slad3 <feba23@live.com>
2020-11-15 18:48:43 -0600

    Adding base functionality to script analyzer component

--- src/app/components/datascience/the-office-script-analyzer/the-office-script-analyzer.component.ts
+++ src/app/components/datascience/the-office-script-analyzer/the-office-script-analyzer.component.ts
@@ -1,15 +1,124 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Request } from '../../../api/request.service';
+
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpEventType,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Observable, Subject } from 'rxjs';
+import * as Highcharts from 'highcharts';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-the-office-script-analyzer',
   templateUrl: './the-office-script-analyzer.component.html',
-  styleUrls: ['./the-office-script-analyzer.component.css']
+  styleUrls: ['./the-office-script-analyzer.component.css'],
 })
 export class TheOfficeScriptAnalyzerComponent implements OnInit {
+  dev: boolean;
+  progress: number;
+  data: Object;
+  backendUrl = 'localhost:8092';
+ 
+  form: FormGroup;
 
-  constructor() { }
+  @Output() newResponse = new EventEmitter<string>();
+  uploadStatus: Observable<number>;
 
-  ngOnInit(): void {
+  instructionsToggle = true;
+  graphsToggle = false;
+
+  constructor(
+    private request: Request,
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+    private spinner: NgxSpinnerService
+  ) {
+
+	this.form = this.formBuilder.group({
+		query: "",
+		person: "" 
+	  });
+
+	if (location.host.toString() === 'localhost:4200') {
+		this.dev = true;
+		this.backendUrl = "http://localhost:8092"
+	  } else {
+		this.dev = false;
+	  }
+
+  }
+
+  ngOnInit(): void {}
+
+  onFileChange(event) {
+	// console.log('change');
+
+	this.form.get('query').setValue(event.target.form[0].value)
+	this.form.get('person').setValue(event.target.form[1].value)
+  }
+
+  onSubmitUpload() {
+    if (this.form.get('query').value !== '' || this.form.get('person').value !== '') {
+      this.graphsToggle = false;
+      this.spinner.show();
+
+      const response = this.request.postQuery(
+		  this.form,
+        this.backendUrl + '/OfficeQuery'
+      );
+
+      this.uploadStatus = response.status;
+
+      response.status.subscribe((event) => {
+        this.progress = event;
+        // this.progressbar.innerHTML = event.toString();
+      });
+      this.data = null;
+      response.file.subscribe((file) => {
+        try {
+          this.newResponse.emit(file);
+          this.data = JSON.parse(file);
+          this.loadGraphs();
+        } catch (error) {
+          alert('Error connecting to backend Server');
+          this.spinner.hide();
+        }
+      });
+    } else {
+      window.alert(
+        `Error`
+      );
+    }
+  }
+
+  loadGraphs() {
+
+    // console.log('loading Graphs');
+    console.log(this.data);
+
+    this.instructionsToggle = false;
+
+
+
+    this.graphsToggle = true;
+    this.spinner.hide();
   }
 
 }

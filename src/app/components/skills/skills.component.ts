35a4f74d3732c431039b3aeb649019c70ef37d43
Slad3 <feba23@live.com>
2021-01-14 02:11:27 -0600

    Updating Other skills

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -14,7 +14,6 @@ export class SkillsComponent implements OnInit {
 
     for (var i = 0; i < acc.length; i++) {
       acc[i].addEventListener('click', function () {
-		  console.log(this)
         this.classList.toggle('active');
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {

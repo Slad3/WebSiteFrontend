f8000d249608c395d2487c4a797c03ab6faec2e5
Slad3 <feba23@live.com>
2021-01-15 16:20:10 -0600

    Updating local angular

--- src/app/components/singlepost/singlepost.component.spec.ts
+++ src/app/components/singlepost/singlepost.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SinglepostComponent } from './singlepost.component';
 
@@ -6,7 +6,7 @@ describe('SinglepostComponent', () => {
   let component: SinglepostComponent;
   let fixture: ComponentFixture<SinglepostComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SinglepostComponent ]
     })
